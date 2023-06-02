cb9e2c2d4485a06312b4aaa99fafe9d2a9db4f43
GkIgor <igorf0660@gmail.com>
2023-06-02 15:25:31 -0300

    Posicionado as elipse, só falta alinhar

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,8 +8,8 @@ import Image from "next/image";
 
 function Login() {
   return (
-    <main className="grid grid-cols-2">
-      <section className="">
+    <section className="grid grid-cols-2">
+      <article className="">
         <div>
           <p>Crie sua Conta</p>
           <Form />
@@ -37,21 +37,21 @@ function Login() {
             </span>
           </div>
         </div>
-      </section>
-      <article className="hidden md:flex flex-row ">
-        <div className="relative">
-          <div className="">
-            <Image src={Elipse49} width={500} height={500} alt="" />
+      </article>
+      <article className="hidden relative md:flex flex-row">
+        <div className="overflow-x-hidden">
+          <div className="absolute top-0 right-0 z-0">
+            <Image src={Elipse49} width={600} height={250} alt="" />
           </div>
-          <div className="">
-            <Image src={Elipse50} width={500} height={500} alt="" />
+          <div className="absolute top-18 right-0 z-10">
+            <Image src={Elipse50} width={471.8} height={471.8} alt="" />
           </div>
-          <div className="">
-            <Image src={ti51} width={500} height={500} alt="" />
+          <div className="absolute top-20 left-0 z-20">
+            <Image src={ti51} width={537.2} height={537.2} alt="" />
           </div>
         </div>
       </article>
-    </main>
+    </section>
   );
 }
 

cf450571bc943b0e62b8e4beab67b471219f09ae
GkIgor <igorf0660@gmail.com>
2023-06-10 16:31:12 -0300

    Adcionado estilos de scrool no css global

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -24,8 +24,9 @@ const links = {
 
 function Login() {
   return (
-    <section className="md:grid md:grid-cols-2 overflow-y-auto">
-      <article className="flex flex-col items-center">
+    <>
+    <section className="md:grid md:grid-cols-2 h-screen overflow-y-auto">
+      <article className="flex flex-col items-center" >
         <div className="mt-[2.5em]">
           <p
             style={{ fontFamily: "'Inter', sans-serif" }}
@@ -56,7 +57,7 @@ function Login() {
             <h2>Ou conecte-se com</h2>
           </div>
 
-          <ul className="flex flex-row gap-6">
+          <ul className="flex flex-row gap-6 ">
             <li className="rounded-full border-2 border-gray-600 p-2 md:p-3 ">
               <div className="pr-[1px]">
                 <Image src={GoogleIcon} width={40} height={40} alt="" />
@@ -85,20 +86,21 @@ function Login() {
       </article>
 
       {/*elipses*/}
-      <article className="hidden relative md:flex">
-        <div className="">
-          <div className="absolute  top-0 right-0 z-0">
-            <Image src={Elipse49} width={433} height={960} alt="" />
+      <article className="hidden relative md:block overflow-y-hidden h-full" >
+
+          <div className="absolute  top-0 right-0 z-0 overflow-hidden">
+            <Image src={Elipse49} width={323} height={350} alt="" className="overflow-y-hidden"/>
           </div>
-          <div className="absolute  top-18 right-0 z-10">
-            <Image src={Elipse50} width={615} height={985} alt="" />
+          <div className="absolute top-18 right-0 z-10 overflow-hidden" >
+            <Image src={Elipse50} width={505} height={375} alt="" className="overflow-y-hidden" />
           </div>
-          <div className="absolute -top-4 z-20">
-            <Image src={ti51} width={904 - 310} height={851 - 310} alt="" />
+          <div className="absolute top-[2.5rem] z-20 right-28 overflow-hidden">
+            <Image src={ti51} width={503} height={503} alt="" className="overflow-y-hidden"/>
           </div>
-        </div>
+
       </article>
     </section>
+    </>
   );
 }
 

765be2eb486e09ba517949269fbe9daae94ae503
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-07-11 14:35:32 -0300

    Changed home

--- src/app/components/MaintenancePage/index.tsx
+++ src/app/components/MaintenancePage/index.tsx
@@ -2,7 +2,8 @@ import Image from "next/image"
 
 export default function MaintenancePage(){
     return (
-        <div className="absolute w-full h-full bg-blue-500 flex flex-col justify-center items-center">
+        <div className="absolute w-full h-full bg-blue-200 flex flex-col justify-center items-center">
+            <Image className="animate-float" alt="Logo da All Stack" src={"/logo.png"} width={250} height={250}></Image>
             <div className="px-5 py-2 max-w-xs border-solid border-blue-800 border-2 bg-slate-400 rounded-lg">
                 <h1 className="w-full text-center font-bold">Atenção!</h1>
                 <p className="text-center">Página em construção. Volte em breve.</p>

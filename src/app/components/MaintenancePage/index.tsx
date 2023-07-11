3602852fd98a32a90a5ec4349dfd4fdc3ae2bbe6
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-07-11 14:24:10 -0300

    first commit

---
+++ src/app/components/MaintenancePage/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+export default function MaintenancePage(){
+    return (
+        <div className="absolute w-full h-full bg-blue-500 flex flex-col justify-center items-center">
+            <div className="px-5 py-2 max-w-xs border-solid border-blue-800 border-2 bg-slate-400 rounded-lg">
+                <h1 className="w-full text-center font-bold">Atenção!</h1>
+                <p className="text-center">Página em construção. Volte em breve.</p>
+            </div>
+        </div>
+    )
+}
